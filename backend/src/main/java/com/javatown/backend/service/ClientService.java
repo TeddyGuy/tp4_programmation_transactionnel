cbb93f579dad4b1b4d6c7e29d5fd40404d3eccc7
Teddy Guy <mediteddyguy@gmail.com>
2022-04-25 22:41:04 -0400

    service des clients

---
+++ backend/src/main/java/com/javatown/backend/service/ClientService.java
@@ -0,0 +1,25 @@
+package com.javatown.backend.service;
+
+import com.javatown.backend.repository.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientService {
+    private ClientRepository clientRepository;
+    private DocumentLoanRepository documentLoanRepository;
+    private BookRepository bookRepository;
+    private CDRepository cdRepository;
+    private DVDRepository dvdRepository;
+
+    public ClientService(ClientRepository clientRepository,
+                        DocumentLoanRepository documentLoanRepository,
+                        BookRepository bookRepository,
+                        CDRepository cdRepository,
+                        DVDRepository dvdRepository) {
+        this.clientRepository = clientRepository;
+        this.documentLoanRepository = documentLoanRepository;
+        this.bookRepository = bookRepository;
+        this.cdRepository = cdRepository;
+        this.dvdRepository = dvdRepository;
+    }
+}
