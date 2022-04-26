3c46cde02daddd1e4aea291bcd54e8f6e57423ab
Teddy Guy <mediteddyguy@gmail.com>
2022-04-25 22:27:41 -0400

    repository des livres

---
+++ backend/src/main/java/com/javatown/backend/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.javatown.backend.repository;
+
+import com.javatown.backend.model.document.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
